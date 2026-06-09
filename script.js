b5027e979ef10b5e2dc93fab86390a6acd1b9278
vikaschhillar25-glitch <vikaschhillar.25@gmail.com>
2026-06-09 18:19:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function myfunction(){
-    var a =4;
-  document.getElementById("demo").innerHTML=a*a;  
+function showTime(){
+    var date =newDate();
+    var h =getHour();
+    var m =getMinutes();
+    var s =getseconds();
+var session  ="AM";
+
+if(h == 0){
+    h= 12;
 }
+
+if(h>12){
+    session = "PM";
+}
+h = (h < 10) ? "0 " + h : h;
+m = (m < 10) ? "0 " +  m : m ; 
+ s = (s < 10) ? "0 " + s : s ;
+
+ var time =h+":"+m+":"+s+""+session
+ document.getElementById("MyClockDisplay").innerText = time;
+ srtTimeout(showTime,1000);
+
+}
+showTime();
